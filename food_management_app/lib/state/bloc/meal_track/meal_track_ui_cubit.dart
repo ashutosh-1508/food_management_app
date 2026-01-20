43362c92732e704d346b274f5c00da4715afbdd3
ashutoshw2001 <ashutoshw2001@gmail.com>
2026-01-20 18:14:40 +0530

    updated this cubit file for toggling between the plans on meal track screen

--- food_management_app/lib/state/bloc/meal_track/meal_track_ui_cubit.dart
+++ food_management_app/lib/state/bloc/meal_track/meal_track_ui_cubit.dart
@@ -1,23 +1,28 @@
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import '../../../data/models/meal_plan.dart';
 
 class MealTrackUiState extends Equatable {
   final int selectedDayIndex;
+  final MealPlan? selectedPlan;
 
   const MealTrackUiState({
     required this.selectedDayIndex,
+    this.selectedPlan,
   });
 
   MealTrackUiState copyWith({
     int? selectedDayIndex,
+    MealPlan? selectedPlan,
   }) {
     return MealTrackUiState(
       selectedDayIndex: selectedDayIndex ?? this.selectedDayIndex,
+      selectedPlan: selectedPlan ?? this.selectedPlan,
     );
   }
 
   @override
-  List<Object?> get props => [selectedDayIndex];
+  List<Object?> get props => [selectedDayIndex, selectedPlan];
 }
 
 class MealTrackUiCubit extends Cubit<MealTrackUiState> {
@@ -25,5 +30,9 @@ class MealTrackUiCubit extends Cubit<MealTrackUiState> {
 
   void selectDay(int index) {
     emit(state.copyWith(selectedDayIndex: index));
+  }
+
+  void selectPlan(MealPlan plan) {
+    emit(state.copyWith(selectedPlan: plan));
   }
 }
